a8ba53893e2a8550f802014277139759abe3df92
Rajsekhor Saikia <rajsekhor.saikia.2001@gmail.com>
2023-05-30 16:54:56 +0530

    v0.0.2: Basic ejs files, controller, partials and mongoose added

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
 const app = express();
 const port = 8000;
 
 app.use('/', require('./routes'));
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, (err) => {
     if (err) console.log(`Error in running server: ${err}`);
