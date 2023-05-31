d9d60b461d9ae48a825ecc63d0d1cd9595f79103
Rajsekhor Saikia <rajsekhor.saikia.2001@gmail.com>
2023-05-31 11:26:50 +0530

    v0.2.0: Post and Comment added

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,28 @@
-module.exports.home = (req, res) => {
-    return res.render('home', {
-        title: "Home"
+const Post = require("../models/post");
+const User = require("../models/user");
+
+ module.exports.home = function (request, response) {
+  console.log("Home controller action is working");
+
+  // Populate the user of each post.
+  Post.find({})
+    .populate("user")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+      },
+    })
+    .exec(function (error, posts) {
+      User.find({}, function (error, users) {
+        if (error) {
+          return response.render("back");
+        }
+        return response.render("home", {
+          title: " Home",
+          posts: posts,
+          all_users: users,
+        });
+      });
     });
 };
