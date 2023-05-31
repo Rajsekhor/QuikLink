c01628da2888373980ecfadc13fe4b378b9d4c43
Rajsekhor Saikia <rajsekhor.saikia.2001@gmail.com>
2023-05-31 10:00:31 +0530

    v0.1.0: User Authentication added with passport js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,64 @@
+const User = require('../models/user');
+
 module.exports.profile = (req, res) => {
-    res.end('<h1>User Profile</h1>');
+    res.render('user_profile', {
+        title: "User Profile"
+    });
 };
+
+//render the Sign Up
+module.exports.signUp = (req, res) => {
+    if (req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
+
+    res.render('user_sign_up', {
+        title: "QuikLink | Sign Up"
+    });
+};
+
+//render the Sign In
+module.exports.signIn = (req, res) => {
+    if (req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
+
+    res.render('user_sign_in', {
+        title: "QuikLink | Sign In",
+    });
+};
+
+//get the sign up data
+module.exports.create = (req, res) => {
+    if (req.body.password != req.body.confirm_password) {
+        return res.redirect('back');
+    }
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (err) { console.log('error in finding user'); return; }
+        if (!user) {
+            User.create(req.body, (err, user) => {
+                if (err) { console.log('error in finding user'); return; }
+
+                res.redirect('/users/sign-in');
+            });
+        }
+        else {
+            res.redirect('back');
+        }
+    });
+};
+
+//sign in and create session for user
+module.exports.createSession = (req, res) => {
+    res.redirect('/');
+};
+
+module.exports.destroySession = function (req, res) {
+    // logout has been upgraded as an asynchronous function so it requires a callback function to handle error now
+    req.logout(function (error) {
+      if (error) {
+        return next(error);
+      }
+      return res.redirect("/");
+    });
+  };
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require("connect-mongo");
 
 const app = express();
 const port = 8000;
 
-app.use('/', require('./routes'));
+app.use(express.urlencoded());
+app.use(cookieParser());
 app.use(express.static('./assets'));
 
 app.use(expressLayouts);
@@ -15,6 +21,33 @@ app.set('layout extractScripts', true);
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.use(session({
+    name: 'quiklink',
+    //TODO change secretbefore deployment 
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: MongoStore.create({
+        mongooseConnection: db,
+        autoRemove: "disabled",
+        // Added next line from stackoverflow to remove the (session) parameter from line 13
+        mongoUrl: 'mongodb://localhost/quiklink'
+      }),function(error){
+        console.log(error || 'connect-mongodb setup okay');
+      }
+    })
+  );
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+app.use('/', require('./routes'));
 
 app.listen(port, (err) => {
     if (err) console.log(`Error in running server: ${err}`);
