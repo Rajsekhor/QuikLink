95e2e284029b34d12f8c3f40c7033023f0060152
Rajsekhor Saikia <rajsekhor.saikia.2001@gmail.com>
2023-06-02 14:24:14 +0530

    v0.2.5: User Avatar feature to User Profile added

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ app.use(express.static("./assets"));
 
 app.use(expressLayouts);
 
+app.use("/uploads",express.static(__dirname + "/uploads"));
+
 // extract style and scripts from sub pages into the layout
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
