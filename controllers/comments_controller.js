2d06410e9889e298cf36a10c8ad75a09da5f6d9e
Rajsekhor Saikia <rajsekhor.saikia.2001@gmail.com>
2023-06-08 13:05:48 +0530

    v0.3.4: Implementation of AJAX for creating Comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,43 +1,61 @@
+// const Comment = require("../models/comment");
+// const Post = require("../models/post");
+// const commentsMailer = require('../mailers/comments_mailer');
+// const Like = require('../models/like');
+
+// module.exports.create = async function (req, res) {
+//   try {
+//     let post = await Post.findById(req.body.post);
+//     if (post) {
+//       let comment = await Comment.create({
+//         content: req.body.content,
+//         post: req.body.post,
+//         user: req.user._id,
+//       });
+
+//       post.comments.push(comment);
+//       post.save();
+//       req.flash("success","Comment published!");
+//       comment = await comment.populate('user', 'name email');
+//       commentsMailer.newComment(comment);
+//       res.redirect("/");
+//     }
+//     else {
+//       req.flash("error","You cannot publish this comment!");
+//       return response.redirect("back");
+//     }
+//   } catch (error) {
+//     req.flash("error",error);
+//     return;
+//   }
+// };
+
+// module.exports.destroy = async function (req, res) {
+//   try {
+//     let comment = await Comment.findById(req.params.id);
+//     if (comment.user == req.user.id) {
+//       let postId = comment.post;
+//       comment.remove();
+//       let post = await Post.findByIdAndUpdate(postId, {
+//         $pull: { comments: req.params.id },
+//       });
+//       await Like.deleteMany({likeable: comment._id, onModel: 'Comment'});
+//       req.flash("success","Comment deleted!");
+//       return res.redirect("back");
+//     } else {
+//       req.flash("error","You cannot delete this comment!");
+//       return res.redirect("back");
+//     }
+//   } catch (error) {
+//     req.flash("error",error);
+//     return;
+//   }
+// };
+
 const Comment = require("../models/comment");
 const Post = require("../models/post");
 const commentsMailer = require('../mailers/comments_mailer');
 const Like = require('../models/like');
-
-// module.exports.create = function (req, res) {
-//   Post.findById(req.body.post, function (error, post) {
-//     if (post) {
-//       Comment.create(
-//         {
-//           content: req.body.content,
-//           post: req.body.post,
-//           user: req.user._id,
-//         },
-//         function (error, comment) {
-//           // handle error
-//           post.comments.push(comment);
-//           post.save();
-
-//           res.redirect("/");
-//         }
-//       );
-//     }
-//   });
-// };
-
-// module.exports.destroy=function(req,res){
-//   Comment.findById(req.params.id,function(err,comment){
-//     if(comment.user=req.user.id){
-//       let postId=comment.post;
-//       comment.remove();
-//       Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}},function(err,post){
-//         return res.redirect('back');
-//       })
-//     }else{
-//       return res.redirect('back')
-//     }
-//   })
-// }
-
 
 module.exports.create = async function (req, res) {
   try {
@@ -51,17 +69,40 @@ module.exports.create = async function (req, res) {
 
       post.comments.push(comment);
       post.save();
-      req.flash("success","Comment published!");
+      req.flash("success", "Comment published!");
       comment = await comment.populate('user', 'name email');
-      commentsMailer.newComment(comment);
-      res.redirect("/");
-    }
-    else {
-      req.flash("error","You cannot publish this comment!");
+      // commentsMailer.newComment(comment);
+
+      if (req.xhr) {
+        return res.status(200).json({
+          data: {
+            comment: comment,
+          },
+          message: "Comment published!",
+        });
+      }
+
+      res.redirect("back");
+    } else {
+      req.flash("error", "You cannot publish this comment!");
+
+      if (req.xhr) {
+        return res.status(400).json({
+          message: "Error publishing comment!",
+        });
+      }
+
       return response.redirect("back");
     }
   } catch (error) {
-    req.flash("error",error);
+    req.flash("error", error);
+
+    if (req.xhr) {
+      return res.status(500).json({
+        message: "Internal Server Error",
+      });
+    }
+
     return;
   }
 };
@@ -75,15 +116,39 @@ module.exports.destroy = async function (req, res) {
       let post = await Post.findByIdAndUpdate(postId, {
         $pull: { comments: req.params.id },
       });
-      await Like.deleteMany({likeable: comment._id, onModel: 'Comment'});
-      req.flash("success","Comment deleted!");
+      await Like.deleteMany({ likeable: comment._id, onModel: 'Comment' });
+      req.flash("success", "Comment deleted!");
+
+      if (req.xhr) {
+        return res.status(200).json({
+          data: {
+            comment_id: req.params.id,
+          },
+          message: "Comment deleted",
+        });
+      }
+
       return res.redirect("back");
     } else {
-      req.flash("error","You cannot delete this comment!");
+      req.flash("error", "You cannot delete this comment!");
+
+      if (req.xhr) {
+        return res.status(400).json({
+          message: "Error deleting comment!",
+        });
+      }
+
       return res.redirect("back");
     }
   } catch (error) {
-    req.flash("error",error);
+    req.flash("error", error);
+
+    if (req.xhr) {
+      return res.status(500).json({
+        message: "Internal Server Error",
+      });
+    }
+
     return;
   }
 };
